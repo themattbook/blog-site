6ec6a5f4526d692480332437626eb3601e56c2e6
Matthew Sweet <themattbook@gmail.com>
2024-01-24 00:53:35 -0600

    Updates to UI, starfield

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('./src/style.css');
+	eleventyConfig.addPassthroughCopy('./src/style-light.css');
 	eleventyConfig.addPassthroughCopy('./src/assets');
 
 	eleventyConfig.addFilter('postDate', (dateObj) => {
